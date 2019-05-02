b9a1c73b0335a2549d5724aad4516c5d71057c2f
Nestor Lopez <nlopezdev@gmail.com>
2019-05-02 01:41:46 -0500

    add tests cases with diferent selectors

--- src/App.test.js
+++ src/App.test.js
@@ -11,5 +11,25 @@ describe('<App />', () => {
   it('should contain 1 p element', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find('p').length).toBe(1);
-  });
+  })
+
+  //class sintax selector
+  it('a element should exist', () => {
+    const wrapper = shallow(<App/>);
+    expect(wrapper.find('.App-link').exists()).toBe(true)
+  })
+
+  //element sintax select
+  it('a element should have class', () => {
+    const wrapper = shallow(<App/>);
+    expect(wrapper.find('a').hasClass('App-link')).toBe(true)
+  })
+
+  //object property selector
+  it('logo should exists', () => {
+    const wrapper = shallow(<App/>);
+    expect(wrapper.find({alt: 'logo'}).exists()).toBe(true)
+  })
+
+
 });
