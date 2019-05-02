65ad55d62826f64844c52c24c94697cad66a3b3f
Nestor Lopez <nlopezdev@gmail.com>
2019-05-01 23:11:50 -0500

    add first test case

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+describe('<App />', () => {
+  it('should contain 1 p element', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('p').length).toBe(1);
+  });
 });
